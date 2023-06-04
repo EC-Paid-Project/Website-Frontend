9223f0473d4d55b193a33a758dacd310e331854b
Syed Owais <owaisali246.soa@gmail.com>
2023-06-04 06:06:03 +0500

    Cart Pages added

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./CartPage.css";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import CartItem from "../components/CartItem";
 
 const CartPage = () => {
+  const [cartItems, setCartItems] = useState(2);
+
   return (
     <div>
       <Navbar />
-      <div className="cart-page-main min-h-screen">Cart Page</div>
+      <div className="cart-page-main min-h-screen">
+        {/* <div className="cart-page-center"> */}
+        <div className="cart-page-section-left">
+          <h2 className="cart-page-section-title">
+            Cart Items ({cartItems} Items)
+          </h2>
+          <div className="cart-page-cart-item">
+            <CartItem />
+          </div>
+        </div>
+        <div className="cart-page-section-right"></div>
+        {/* </div> */}
+      </div>
       <Footer />
     </div>
   );
